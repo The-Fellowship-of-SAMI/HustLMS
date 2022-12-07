9ec4c6b478b2c2fa7ea661926f1b904e88ea925c
ducpham01 <ducpa.01@icloud.com>
2022-12-07 11:26:45 +0700

    add model

---
+++ LetterManagementBackendNoDocker/Models/Letter.cs
@@ -0,0 +1,23 @@
+﻿namespace LetterManagementBackendNoDocker.Models
+{
+    public class Letter
+    {
+        public Guid Id { get; set; }
+
+        public Guid Template { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int ReceiverId { get; set; }
+
+        public int ResolverId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set;}
+
+        public string State { get; set; }
+
+
+    }
+}
---
+++ LetterManagementBackendNoDocker/Models/LetterTemplate.cs
@@ -0,0 +1,19 @@
+﻿namespace LetterManagementBackendNoDocker.Models
+{
+    public class LetterTemplate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<FieldAndValue> FieldAndValues { get; set; }
+    }
+
+    public class FieldAndValue
+    {
+        public int Order { get; set; }
+
+        public bool IsActive { get; set; }
+        public string Field { get; set; }
+        public string Value { get; set; }
+    }
+}
